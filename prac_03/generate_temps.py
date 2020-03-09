3d6d9920fff9588d096550b3ff67faf925fd42b6
Belchy06 <william.belcher@hotmail.com>
2020-03-09 20:40:43 +1000

    Fix temperature generating integer values

--- prac_03/generate_temps.py
+++ prac_03/generate_temps.py
@@ -2,14 +2,14 @@ import random
 
 FILE_NAME = "temps_input.txt"
 NUMBER_OF_TEMPS = 15
-MAX_TEMP = 200
-MIN_TEMP = -200
+MAX_TEMP = 200.00
+MIN_TEMP = -200.00
 
 
 def main():
     out_file = open(FILE_NAME, 'w')
     for i in range(NUMBER_OF_TEMPS):
-        print(random.randint(MIN_TEMP, MAX_TEMP), file=out_file)
+        print(random.uniform(MIN_TEMP, MAX_TEMP), file=out_file)
     out_file.close()
 
 
