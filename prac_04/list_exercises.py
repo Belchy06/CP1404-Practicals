b32d16a22ec51236b3fa75215a8f24e32800f691
Belchy06 <william.belcher@hotmail.com>
2020-03-16 17:20:18 +1000

    Add completed exercise 1

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -3,3 +3,35 @@ CP1404/CP5632 Practical
 Intermediate Exercises, prac_04
 """
 
+
+def exercise_1():
+    number_of_nums = int(input("Enter the amount of numbers you want: "))
+    numbers = get_list_of_numbers(number_of_nums)
+    print_number_facts(numbers)
+
+def get_list_of_numbers(number_of_nums):
+    numbers = []
+    for i in range(1, number_of_nums + 1):
+        valid_number = False
+        while not valid_number:
+            try:
+                number = int(input("Number: "))
+                valid_number = True
+            except:
+                print("Please enter a valid number!")
+        numbers.append(number)
+    return numbers
+
+
+def print_number_facts(list_of_numbers):
+    if len(list_of_numbers) > 0:
+        print("The first number is: {}".format(list_of_numbers[0]))
+        print("The last number is: {}".format(list_of_numbers[len(list_of_numbers) - 1]))
+        print("The smallest number is: {}".format(min(list_of_numbers)))
+        print("The largest number is: {}".format(max(list_of_numbers)))
+        print("The average of the numbers is: {}".format(sum(list_of_numbers) / len(list_of_numbers)))
+    else:
+        print("There are no numbers!")
+
+
+exercise_1()
