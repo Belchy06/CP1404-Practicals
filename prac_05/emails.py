8a8f10c8457f753217aa03870ebb83a32cbaedea
William Belcher <william.belcher@hotmail.com>
2020-03-26 12:01:57 +1000

    Reformat emails script to use functions

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,24 +2,57 @@
 CP1404/CP5632 Practical
 Stores the email and name of a user in a dictionary
 """
-email_dictionary = {}
-email = input("Email: ")
-email_name = email.split('@')
-name = email_name[0].split('.')
-name2 = " ".join(name)
-name3 = name2.title()
-prompt = input("Is your name {}? (y/n): ".format(name3)).lower()
-if prompt[0] == 'y':
-    print('yes')
-    email_dictionary[email] = name3
-elif prompt[0] == 'n':
-    print('n')
-    user_name = input("Name: ")
-    email_dictionary[email] = user_name
-else:
-    print("other")
 
-print(email_dictionary)
 
-for email, name in email_dictionary.items():
-    print("{} ({})".format(name, email))
+def main():
+    email_dictionary = {}
+
+    user_email = get_user_email()
+    while user_email != "":
+        user_name = extract_user_from_email(user_email)
+        confirmed_user_name = confirm_username(user_name)
+        email_dictionary[user_email] = confirmed_user_name
+        user_email = get_user_email()
+
+    print_email_dictionary(email_dictionary)
+
+
+def get_user_email():
+    email = input("Email: ")
+    return email
+
+
+def extract_user_from_email(input_email):
+    split_email = input_email.split('@')
+    name_array = split_email[0].split('.')
+    user_name = " ".join(name_array).title()
+    return user_name
+
+
+def confirm_username(original_name):
+    username = original_name
+    response = input("Is your name {}? (y/n): ".format(original_name)).lower()
+    valid_response = False
+    while not valid_response:
+        if response == "":
+            valid_response = True
+        elif response[0] == 'y':
+            valid_response = True
+        elif response[0] == 'n':
+            specified_name = input("Name: ")
+            while specified_name == "":
+                specified_name = input("Name: ")
+            username = specified_name
+            valid_response = True
+        else:
+            response = input("Is your name {}? (y/n): ".format(original_name)).lower()
+    return username
+
+
+def print_email_dictionary(dictionary):
+    print()
+    for email, name in dictionary.items():
+        print("{} ({})".format(name, email))
+
+
+main()
