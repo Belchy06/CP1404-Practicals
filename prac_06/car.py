5c9426fe52393a196ad72db7740ce231472ce676
Belchy06 <william.belcher@hotmail.com>
2020-04-05 17:18:27 +1000

    Complete modifications section

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,13 +4,14 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
         self.odometer = 0
+        self.name = name
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
@@ -29,3 +30,6 @@ class Car:
             self.fuel -= distance
         self.odometer += distance
         return distance
+
+    def __str__(self):
+        return "{}, fuel = {}, odometer = {}".format(self.name, self.fuel, self.odometer)
--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,7 +6,7 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("Car", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -14,6 +14,16 @@ def main():
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+
+    # Modifications
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print("limo has {} fuel".format(limo.fuel))
+    limo.drive(115)
+    print("limo odo = {}".format(limo.odometer))
+
+    print(my_car)
+    print(limo)
 
 
 main()
