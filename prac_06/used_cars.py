d9d3ee14f4d8070aa1ef31d5cbe866385e6a6db1
Belchy06 <william.belcher@hotmail.com>
2020-04-05 17:08:31 +1000

    Update used_cars base file

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -1,31 +1,19 @@
-"""CP1404/CP5632 Practical - Car class example."""
+"""CP1404/CP5632 Practical - Client code to use the Car class."""
+# Note that the import has a folder (module) in it.
+
+from prac_06.car import Car
 
 
-class Car:
-    """Represent a Car object."""
+def main():
+    """Demo test code to show how to use car class."""
+    my_car = Car(180)
+    my_car.drive(30)
+    print("fuel =", my_car.fuel)
+    print("odo =", my_car.odometer)
+    print(my_car)
 
-    def __init__(self, fuel=0):
-        """Initialise a Car instance.
+    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
+    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
-        fuel: float, one unit of fuel drives one kilometre
-        """
-        self.fuel = fuel
-        self.odometer = 0
 
-    def add_fuel(self, amount):
-        """Add amount to the car's fuel."""
-        self.fuel += amount
-
-    def drive(self, distance):
-        """Drive the car a given distance.
-
-        Drive given distance if car has enough fuel
-        or drive until fuel runs out return the distance actually driven.
-        """
-        if distance > self.fuel:
-            distance = self.fuel
-            self.fuel = 0
-        else:
-            self.fuel -= distance
-        self.odometer += distance
-        return distance
+main()
