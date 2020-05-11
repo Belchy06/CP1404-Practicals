3f5131829da9462c3cc401c6a3f80fb1e4aa34f3
Belchy06 <william.belcher@hotmail.com>
2020-05-11 19:11:58 +1000

    Add renaming logic

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -6,7 +6,6 @@ Cleanup file names
 import shutil
 import os
 
-
 def main():
     """Demo os module functions."""
     print("Starting directory is: {}".format(os.getcwd()))
@@ -17,21 +16,16 @@ def main():
     # Print a list of all files in current directory
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
 
-    # Make a new directory
-    # The next time you run this, it will crash if the directory exists
-    try:
-        os.mkdir('temp')
-    except FileExistsError:
-        print("temp directory already exists")
-
     # Loop through each file in the (current) directory
     for filename in os.listdir('.'):
         # Ignore directories, just process files
         if os.path.isdir(filename):
             continue
 
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
+        #print(split_camel_case(filename))
+
+        print(get_fixed_filename(filename))
+        #rint("Renaming {} to {}".format(filename, new_name))
 
         # TODO: Try these options one at a time
         # Option 1: rename file to new name - in place
@@ -44,7 +38,28 @@ def main():
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    return new_name
+    temp = []
+    for index, char in enumerate(new_name):
+        # If the character is uppercase and not the first in the word
+        if char.isupper() and index != 0:
+            # If the preceding character is an not an underscore, add the underscore in
+            if new_name[index - 1] != "_":
+                temp.append("_")
+                temp.append(char)
+            # The underscore is already there so just append the character normally
+            else:
+                temp.append(char)
+
+        # Else if the current char is lowercase and there is a preceding opening bracket
+        elif new_name[index - 1] == "(" and char.islower():
+            # Capitalize the current char
+            temp.append(char.upper())
+
+        # Else just append the normal character
+        else:
+            temp.append(char)
+
+    return "".join(temp)
 
 
 def demo_walk():
@@ -55,7 +70,6 @@ def demo_walk():
         print("\tcontains subdirectories:", subdirectories)
         print("\tand files:", filenames)
         print("(Current working directory is: {})".format(os.getcwd()))
-
 
         for filename in filenames:
             new_name = get_fixed_filename(filename)
