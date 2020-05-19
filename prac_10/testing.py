eaaa041822157cdf982dcb54202dc3bcc33d1a19
Belchy06 <william.belcher@hotmail.com>
2020-05-19 12:14:36 +1000

    Add completed testing.py modifications

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -9,7 +9,8 @@ from prac_06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    lst = [s] * n
+    return " ".join(lst)
 
 
 def is_long_word(word, length=5):
@@ -22,7 +23,7 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
 
 
 def run_tests():
@@ -32,8 +33,6 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
-    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
@@ -41,28 +40,24 @@ def run_tests():
     test_car = Car()
     assert test_car.odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
-    # Note that Car's __init__ function sets the fuel in one of two ways:
-    # using the value passed in or the default
-    # You should test both of these
     test_car = Car(fuel=10)
+    assert test_car.fuel == 10, "Car sets the fuel correctly"
 
 
 run_tests()
+doctest.testmod()
 
-# TODO: 3. Uncomment the following line and run the doctests
-# (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
 
-# TODO: 4. Fix the failing is_long_word function
-# (don't change the tests, change the function!)
-
-# TODO: 5. Write and test a function to format a phrase as a sentence,
-# starting with a capital and ending with a single full stop.
-# Important: start with a function header and just use pass as the body
-# then add doctests for 3 tests:
-# 'hello' -> 'Hello.'
-# 'It is an ex parrot.' -> 'It is an ex parrot.'
-# and one more you decide (one that is valid!)
-# test this and watch the tests fail
-# then write the body of the function so that the tests pass
+def format_phrase(phrase):
+    """
+    >>> format_phrase("hello")
+    'Hello.'
+    >>> format_phrase("It is an ex parrot.")
+    'It is an ex parrot.'
+    >>> format_phrase("he ran very fast")
+    'He ran very fast.'
+    """
+    phrase = phrase.capitalize()
+    if phrase[len(phrase) - 1] != '.':
+        phrase += "."
+    return phrase
